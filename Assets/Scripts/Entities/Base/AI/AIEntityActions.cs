dd1de318243fe32fd7c9c8700a8f59731ca33ab2
ATPEngie <paultresco@gmail.com>
2024-08-21 11:59:52 +0300

    Third commit: added OnExit function to BT nodes and started work on Action scripts

--- Assets/Scripts/Entities/Base/AI/AIEntityActions.cs
+++ Assets/Scripts/Entities/Base/AI/AIEntityActions.cs
@@ -1,9 +1,9 @@
 using System.Numerics;
-
+//we might have a stationary AI, so no movement here
 public class AIEntityActions : EntityActions
 {
     public void FaceTarget(Vector3 pos)
     {
-        //turn to look at pos
+        //turn to look at a position
     }
 }
--- Assets/Scripts/Entities/Base/AI/MobActions.cs
+++ Assets/Scripts/Entities/Base/AI/MobActions.cs
@@ -4,17 +4,19 @@ using UnityEngine.AI;
 public class MobActions : AIEntityActions
 {
     //mob refers to a mobile AI
+    //we are not putting anything attack related here for now,
+    //because we might have something passive like the sack rats
     [SerializeField]
     protected NavMeshAgent agent;
     protected void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
     }
-    public void Stop()
+    protected void Stop()
     {
         agent.isStopped = true;
     }
-    public void MoveToLocation(Vector3 vector3, bool affectrotation = true)
+    protected void MoveToLocation(Vector3 vector3, bool affectrotation = true)
     {
         agent.updateRotation = affectrotation;
         agent.isStopped = false;
