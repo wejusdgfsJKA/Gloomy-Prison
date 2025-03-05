33fd020d5ab2f884ce7cbdb741e90769e460d3b0
AnaStef28 <anamariastefanescu2004@gmail.com>
2025-03-05 21:51:27 +0200

    UI improved + small hover animation for buttons.

---
+++ Assets/Scripts/Player/UI/ButtonHoverEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Vector3 ogButtonSize;
+    public float scalar = 1.1f; //growth factor
+
+    private void Start()
+    {
+        ogButtonSize = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) //hovering
+    {
+        transform.localScale = ogButtonSize * scalar; //button gets a lil excited
+    }
+
+    public void OnPointerExit(PointerEventData eventData) //not hovering
+    {
+        transform.localScale = ogButtonSize; //reset size
+    }
+}
