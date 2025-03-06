ddc0eae861cf7660092f35de485a566a6dedbb56
PT <103441012+wejusdgfsJKA@users.noreply.github.com>
2025-03-06 12:45:09 +0300

    Changed button scalar size from public to SerializeField protected. Added XML comments.

--- Assets/Scripts/Player/UI/ButtonHoverEffect.cs
+++ Assets/Scripts/Player/UI/ButtonHoverEffect.cs
@@ -1,22 +1,25 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private Vector3 ogButtonSize;
-    public float scalar = 1.1f; //growth factor
-
-    private void Start()
+    /// <summary>
+    /// Original size of the button.
+    /// </summary>
+    protected Vector3 ogButtonSize;
+    /// <summary>
+    /// How much the button will increase in size when hovered on.
+    /// </summary>
+    [SerializeField]
+    protected float scalar = 1.1f;
+    private void OnEnable()
     {
         ogButtonSize = transform.localScale;
     }
-
     public void OnPointerEnter(PointerEventData eventData) //hovering
     {
         transform.localScale = ogButtonSize * scalar; //button gets a lil excited
     }
-
     public void OnPointerExit(PointerEventData eventData) //not hovering
     {
         transform.localScale = ogButtonSize; //reset size
